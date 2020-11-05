9432d5c4f156cf5b4b13afbfb4ffe64e6b4f5f6c
Penny Thomopoulou <penyamy@gmail.com>
2020-11-05 17:13:23 +0000

    Add project files.

---
+++ Jeremy'sCuteTimeLoggingApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Jeremy_sCuteTimeLoggingApp
+{
+    public partial class App : Application
+    {
+        static App()
+        {
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId).WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .Build();
+        }
+
+        // Below are the clientId (Application Id) of your app registration and the tenant information.
+        // You have to replace:
+        // - the content of ClientID with the Application Id for your app registration
+        // - the content of Tenant by the information about the accounts allowed to sign-in in your application:
+        //   - For Work or School account in your org, use your tenant ID, or domain
+        //   - for any Work or School accounts, use `organizations`
+        //   - for any Work or School accounts, or Microsoft personal account, use `common`
+        //   - for Microsoft Personal account, use consumers
+        private static string ClientId = "2068be73-3d10-4717-a80b-d456f2687f7d";
+
+        private static string Tenant = "common";
+
+        private static IPublicClientApplication _clientApp;
+
+        public static IPublicClientApplication PublicClientApp { get { return _clientApp; } }
+    }
+}
